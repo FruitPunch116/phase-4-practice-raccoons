3e375c80d8e18b6a27d152a785f641a579a1229e
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-09-20 16:42:41 -0400

    create raccoon route, model

--- server/app.py
+++ server/app.py
@@ -21,5 +21,15 @@ def index():
 
 # write your routes here!
 
+@app.delete('/raccoons/<int:id>')
+def delete_raccoon_by_id (id):
+    try:
+        raccoon = Raccoon.query.filter(Raccoon.id == id).first()
+        db.session.delete(raccoon)
+        db.commit()
+        return {}, 204
+    except:
+        return ({'error: raccoon not found'}), 404
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,12 +11,42 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 class Raccoon(db.Model, SerializerMixin):
-    pass
+    __tablename__ = 'raccoons'
 
+    id = db.Column ( db.Integer, primary_key = True)
+
+    name = db.Column ( db.String, nullable =False, unique = True )
+    age = db.Column ( db.Integer, nullable =False )
+
+    @validates('age')
+    def validate_age ( self, key, value ):
+        if value > 0:
+            return value
+        else:
+            raise ValueError ('Your Raccoon has to be at least one year old.')
+        
+    visits = db.relationship ( 'Visit', back_populates = 'raccoon' )
+    trashcan = association_proxy('visits', 'trashcan')
 
 class Visit(db.Model, SerializerMixin):
-    pass
+    __tablename__ = 'visits'
 
+    id = db.Column ( db.Integer, primary_key = True)
+
+    date = db.Column ( db.String, nullable =False )
+
+    raccoon_id = db.Column ( db.Integer, db.ForeignKey('raccoons.id') )
+    trashcan_id = db.Column ( db.Integer, db.ForeignKey('trashcans.id') )
+
+    raccoon = db.relationship ( 'Raccoon', back_populates = 'visits' )
+    trashcan = db.relationship ( 'Trashcan', back_populates = 'visits' )
 
 class Trashcan(db.Model, SerializerMixin):
-    pass
+    __tablename__ = 'trashcans'
+   
+    id = db.Column ( db.Integer, primary_key = True)
+
+    address = db.Column ( db.String, nullable =False )
+
+    visits = db.relationship ( 'Visit', back_populates = 'trashcan' )
+    raccoon = association_proxy('visits', 'raccoon')
